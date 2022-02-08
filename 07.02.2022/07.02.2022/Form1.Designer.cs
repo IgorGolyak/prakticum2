278418f75b8c48ae428d6c5bf932d9c4988c7c31
IgorGolyak <golyak.03@inbox.ru>
2022-02-08 21:55:50 +0700

    Praktikum2

---
+++ 07.02.2022/07.02.2022/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace _07._02._2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(550, 333);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(793, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 413);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(602, 47);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "SISINYS";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(602, 85);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(63, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "COSOЙ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Построить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(602, 153);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(602, 205);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 539);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ 07.02.2022/07.02.2022/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07._02._2022
+{
+    public partial class Form1 : Form
+    {
+        public static int max_x = 550;
+        public static int max_y = 310;
+        public static int x0 = 10;
+        public static int y0 = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen p = new Pen(Color.Black);
+            g.DrawLine(p, x0, y0 + max_y / 2, x0 + max_x, y0 + max_y / 2);
+            g.DrawLine(p, x0, y0, x0, y0 + max_y);
+            double[] mas_x = new double[100];
+            double[] mas_y = new double[100];
+            dataGridView1.RowCount = 100;
+            for (int i = 0; i < 100; i++)
+            {
+                mas_x[i] = i;
+                if (radioButton1.Checked)
+                {
+                    mas_y[i] = Math.Sin(i / 0.01);
+                    mas_y[i] = Math.Sqrt(i);
+                }
+                else
+                {
+                    mas_y[i] = Math.Cos(i);
+                    mas_y[i] = i * 2 - 3;
+                    if (i != 0)
+                    {
+                        mas_y[i] = 6 / i;
+                    }
+                    else
+                    {
+                        mas_y[0] = 6 / 1;
+                    }
+                }
+                dataGridView1[0, i].Value = mas_y[i];
+                dataGridView1[0, i].Value = mas_x[i];
+            }
+            int kx = 3;
+            int ky = 3;
+            for (int i = 1; i < 100; i++)
+            {
+                int x1_1 = (Convert.ToInt32(mas_x[i - 1]) * kx + x0);
+                int y1_1 = (-(Convert.ToInt32(mas_y[i - 1]) * ky) + y0 + max_y / 2);
+                int x1_2 = (Convert.ToInt32(mas_x[i]) * kx + x0);
+                int y1_2 = (-(Convert.ToInt32(mas_y[i]) * ky) + y0 + max_y / 2);
+                g.DrawLine(p, x1_1, y1_1, x1_2, y1_2);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = max_x;
+            int k1 = Convert.ToInt32(numericUpDown1.Value);
+            int k2 = Convert.ToInt32(numericUpDown2.Value);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen p = new Pen(Color.Black);
+            g.DrawLine(p, x0, y0 + max_y / 2, x0 + max_x, y0 + max_y / 2);
+            g.DrawLine(p, x0, y0, x0, y0 + max_y);
+            double[] mas_y = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                if (radioButton1.Checked)
+                {
+                    mas_y[i] = Math.Round(k1 * Math.Sin(k2 * Math.PI / 100 * i));
+                }
+                else
+                {
+                    mas_y[i] = Math.Round(k1 * Math.Cos(k2 * Math.PI / 100 * i));
+                }
+
+            }
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                g.DrawLine(p,
+                    x0 + i,
+                    y0 - Convert.ToInt32(mas_y[i]) + max_y / 2,
+                    x0 + i + 1,
+                    y0 - Convert.ToInt32(mas_y[i + 1]) + max_y / 2);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 10;
+            numericUpDown2.Value = 10;
+            numericUpDown1.Visible = false;
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Visible = true;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Visible = false;
+        }
+}   }
